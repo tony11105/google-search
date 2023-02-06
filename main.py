68fab3800aef4c3d601440e27df43b0ce7effb8f
tony <s290970178@gmail.com>
2023-02-06 09:55:09 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+API_KEY = ""
+SEARCH_ENGINE_ID = ""
+
+# the search query you want
+query = "Adrienne Francesca Ong Soliven National Taiwan University of Science and Technology linkedin"
+# using the first page
+page = 1
+# constructing the URL
+# doc: https://developers.google.com/custom-search/v1/using_rest
+# calculating start, (page=2) => (start=11), (page=3) => (start=21)
+start = (page - 1) * 10 + 1
+print("Search: ", query)
+url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}"
+
+# make the API request
+data = requests.get(url).json()
+
+# get the result items
+search_items = data.get("items")
+# iterate over 10 results found
+for i, search_item in enumerate(search_items, start=1):
+    try:
+        long_description = search_item["pagemap"]["metatags"][0]["og:description"]
+    except KeyError:
+        long_description = "N/A"
+    # get the page title
+    title = search_item.get("title")
+    # page snippet
+    snippet = search_item.get("snippet")
+    # alternatively, you can get the HTML snippet (bolded keywords)
+    html_snippet = search_item.get("htmlSnippet")
+    # extract the page url
+    link = search_item.get("link")
+    # print the results
+    print("="*10, f"Result #{i+start-1}", "="*10)
+    print("Title:", title)
+    print("Description:", snippet)
+    print("Long description:", long_description)
+    print("URL:", link, "\n")
